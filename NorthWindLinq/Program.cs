061817c82b34dbc80c14352f35f739943aa1f78c
Ryan Dustin Wright <ryandustinwright@hotmail.com>
2020-04-29 15:44:42 -0400

    Created models from an existing database in entity framework core.  Eager load customer list with order, details, and products using LINQ lambda expressions.  Recordit animated GIF of execution added.

--- NorthWindLinq/Program.cs
+++ NorthWindLinq/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ConsoleTables;
+using Microsoft.EntityFrameworkCore;
+using NorthWindLinq.Models;
 
 namespace NorthWindLinq
 {
@@ -6,7 +11,34 @@ namespace NorthWindLinq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("NORTHWIND LINQ EXAMPLES!");
+
+            // Create a DB context
+            using var ctx = new NorthwindContext();
+
+            // Eager Load of Multiple Related Entities with the Include() Method
+            // LINQ lambda expression as a parameter in the Include method
+            // Fill the Customer List with all Orders and Details
+            IList<Customers> Customer = ctx.Customers
+                        .Include(customer => customer.Orders)
+                        .ThenInclude(order => order.OrderDetails)
+                        .ThenInclude(orderdetails => orderdetails.Product)
+                        .ToList();
+
+            // Output to Console
+            Console.WriteLine("CUSTOMERS");
+            ConsoleTable.From(Customer.Where(c => c.CustomerId=="FRANK")).Write();
+
+            Console.WriteLine("\nORDERS");
+            ConsoleTable.From(Customer.SelectMany(x => x.Orders).Where(o => o.OrderId == 10267)).Write();
+
+            Console.WriteLine("\nORDER DETAILS");
+            ConsoleTable.From(Customer.SelectMany(x => x.Orders.SelectMany(y => y.OrderDetails).Where(od => od.OrderId == 10267))).Write();
+
+            Console.WriteLine("\nPRODUCTS");
+            ConsoleTable.From(Customer.SelectMany(x => x.Orders.SelectMany(y => y.OrderDetails.Where(od => od.OrderId == 10267).Select(z => z.Product)))).Write();
+
+            Console.ReadKey();
         }
     }
 }
