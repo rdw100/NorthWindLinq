8a63f33542e46acf76491048338039b6f32b394f
Ryan Dustin Wright <ryandustinwright@hotmail.com>
2020-04-29 14:43:46 -0400

    Add project files.

---
+++ NorthWindLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NorthWindLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
